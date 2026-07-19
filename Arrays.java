0bc64ef7a6edd671513f14ee6ced4b9c6cfb633c
pinnintisaranya25csm-afk <pinnintisaranya.25.csm@anits.edu.in>
2026-07-19 23:05:23 +0530

    Add files via upload

---
+++ Arrays.java
@@ -0,0 +1,25 @@
+package mypackage;
+
+public class Arrays {
+
+	public static void main(String[] args) {
+		int x[]= {10,20,30,40,50};
+			for(int i=0;i<x.length;i++) {
+				System.out.println(x[i]);
+			}
+		   for(int b:x) {
+			   System.out.println(b*b);
+		   }
+		   String  y[]= {"saranya","sarah","dunk","willy"};
+		   int i=0;
+		   while(i<y.length) {
+			   System.out.println(y[i]);
+			   i++;
+			   
+			   
+		   }
+		   
+
+	}
+
+}
